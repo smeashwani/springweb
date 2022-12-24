2eaa63f4b0c8ad7b3bd356f5e6803b8c0139109b
smeashwani <104685977+smeashwani@users.noreply.github.com>
2022-12-24 12:18:49 +0530

    Initial commit

---
+++ src/main/java/springweb/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package springweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+	
+	@RequestMapping("/")
+	public String display() {
+		return "index";
+	}
+}
