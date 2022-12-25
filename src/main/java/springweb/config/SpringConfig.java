0d47d9c98f5aa6cbbfc4039e436a8d7448dbaf5c
smeashwani <104685977+smeashwani@users.noreply.github.com>
2022-12-25 11:16:57 +0530

    annotation

---
+++ src/main/java/springweb/config/SpringConfig.java
@@ -0,0 +1,25 @@
+package springweb.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@EnableWebMvc
+@ComponentScan(basePackages = "springweb.controller")
+public class SpringConfig implements WebMvcConfigurer {
+
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/JSP/");
+		viewResolver.setSuffix(".jsp");
+
+		return viewResolver;
+	}
+
+}
--- src/main/java/springweb/controller/HelloWorldController.java
+++ src/main/java/springweb/controller/HelloWorldController.java
@@ -1,5 +1,8 @@
 package springweb.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelloWorldController {
 	
 	@RequestMapping("/")
-	public String display() {
+	public String display(HttpServletRequest req, HttpServletResponse res) {
 		
 		System.out.println("Controller");
 		return "index";
