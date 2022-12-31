b340bfef9e99796852754ad5243d917c0d74e786
smeashwani <104685977+smeashwani@users.noreply.github.com>
2022-12-31 11:56:46 +0530

    Fields

--- src/main/java/springweb/model/Reservation.java
+++ src/main/java/springweb/model/Reservation.java
@@ -4,6 +4,10 @@ public class Reservation {
 
 	private String firstName;
 	private String lastName;
+	private String gender; 
+	private String[] food;
+	private String cityFrom;  
+	private String cityTo;
 
 	public Reservation() {
 	}
@@ -22,6 +26,38 @@ public class Reservation {
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String[] getFood() {
+		return food;
+	}
+
+	public void setFood(String[] food) {
+		this.food = food;
+	}
+
+	public String getCityFrom() {
+		return cityFrom;
+	}
+
+	public void setCityFrom(String cityFrom) {
+		this.cityFrom = cityFrom;
+	}
+
+	public String getCityTo() {
+		return cityTo;
+	}
+
+	public void setCityTo(String cityTo) {
+		this.cityTo = cityTo;
 	}
 	
 }
