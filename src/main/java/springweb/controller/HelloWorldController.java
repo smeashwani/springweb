d3a4be355469c1772697a5ca51b61748ef2918b6
smeashwani <104685977+smeashwani@users.noreply.github.com>
2023-08-13 10:55:45 +0530

    Update

--- src/main/java/springweb/controller/HelloWorldController.java
+++ src/main/java/springweb/controller/HelloWorldController.java
@@ -1,10 +1,10 @@
 package springweb.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class HelloWorldController {
