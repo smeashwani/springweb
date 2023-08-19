0464fbeef49fc4dac4d5477818a8649237af1d8a
smeashwani <104685977+smeashwani@users.noreply.github.com>
2023-08-19 12:21:07 +0530

    Update

--- src/main/java/springweb/config/WebServletConfiguration.java
+++ src/main/java/springweb/config/WebServletConfiguration.java
@@ -1,15 +1,16 @@
 package springweb.config;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
-
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRegistration;
+
+
+
 public class WebServletConfiguration implements WebApplicationInitializer {
-	@Override
 	public void onStartup(ServletContext ctx) throws ServletException {
 		AnnotationConfigWebApplicationContext annWebConfig = new AnnotationConfigWebApplicationContext();
 		annWebConfig.register(SpringConfig.class);
